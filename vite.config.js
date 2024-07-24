12aa189e87cbe221fa79da551a51a3f4c4212ebd
HuyVo1231 <vonhathuy1306@gmail.com>
2024-07-24 15:15:44 +0700

    part 3 fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/' // Thêm cấu hình này để đảm bảo base path đúng
 })
