ad552667d036d8b7ea08e35ec91c86ff96017c1b
HuyVo1231 <vonhathuy1306@gmail.com>
2024-07-24 15:02:30 +0700

    fix route part 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,17 @@ import Login from './pages/Login/Login'
 import SignUp from './pages/SignUp/SignUp'
 import HomePage from './pages/HomePage/HomePage'
 
-const routes = (
-  <Router>
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/dashboard' element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/signup' element={<SignUp />} />
-    </Routes>
-  </Router>
-)
-
 const App = () => {
-  return <div>{routes}</div>
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/dashboard' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
